fafd29098762b6051edf91e6aab7d10c48c8b2a3
y3hya <tmp.yahya1@gmail.com>
2022-12-14 11:45:58 -0700

    Understanding templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'demo-project';
+
+  color = 'red';
+
+  thecssiwant = 'background-color: ' + this.color;
+
+  theColorHasChangedFunction() {
+    this.thecssiwant = 'background-color: ' + this.color;
+  }
 }
